3de2b1568dcdc4af5acd01d2bd04836a130badb7
Bharat <bharatsinghchaudhary3@gmail.com>
2025-06-09 11:33:43 +0530

    Initial commit

---
+++ oddeven.java
@@ -0,0 +1,13 @@
+public class oddeven{
+    public static void main(String[] args){
+        System.out.println("Even and odd numbers from 1 to 100:");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is even");
+            }
+            else{
+                System.out.println(i + "is odd");
+            }
+        }
+    }
+    }
